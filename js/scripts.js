25fe742c928f7989fa63f3e90a8eaa26f5a1124a
Tyler Ferrin <tyferrin@gmail.com>
2016-12-29 13:41:41 -0800

    add slick plugin

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,39 @@
 $(document).ready(function() {
 
-  $.backstretch([
-    "img/backexample.jpg",
-    "img/image1.jpg",
-    "img/image2.jpg"
+  $(".index-wrap").backstretch([
+    "img/back2.jpg",
+    "img/back1.jpg",
+    "img/back3.jpg",
+    "img/back4.jpg",
+    "img/back5.jpg"
   ], {
     duration: 5000,
     fade: 500
-    });
-
-  $("#camera_menu_click").click(function() {
-    $("#menu_display").toggle();
   });
 
+  $(".people").slick({
+    adaptiveHeight: true,
+    autoplay: false,
+    dots: true,
+    slidesToScroll: 2,
+    slidesToShow: 2,
+
+
+  });
+
+
+
+  $("#camera_menu_click").click(function() {
+    $("div.nav-row").fadeOut(450, function() {
+      $("#menu_display").fadeIn(450);
+    });
+  });
+
+
   $("#menu_close").click(function() {
-    $("#menu_display").toggle();
+    $("#menu_display").fadeOut(450, function() {
+      $("div.nav-row").fadeIn(450);
+    });
   });
 
 });
